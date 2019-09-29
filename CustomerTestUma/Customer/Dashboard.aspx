bfd415f5b33b72e74b3160a7b9092437e6389501
Uma Sanodiya <umasanodiya@outlook.com>
2019-09-29 21:03:13 +0530

    Change for Dashboard

--- CustomerTestUma/Customer/Dashboard.aspx
+++ CustomerTestUma/Customer/Dashboard.aspx
@@ -4,9 +4,13 @@
 
     <div class="row">
                 <div class="col-sm-3"> <asp:Button ID="btnBack" runat="server" Text="Create Customer" CssClass="btn btn-primary" OnClientClick="window.location.href = 'CustomerCreate.aspx';return false;"/></div>
+                <div class="col-sm-3"> <asp:Button ID="btnBack1" runat="server" Text="Customer Data" CssClass="btn btn-primary" OnClientClick="window.location.href = 'CustomerDetails.aspx';return false;"/></div>
+                <div class="col-sm-3"> <asp:Button ID="Button2" runat="server" Text="Customer Update" CssClass="btn btn-primary" OnClientClick="window.location.href = 'UpdateCustomer.aspx';return false;"/></div>            
                 <div class="col-sm-3" style="background-color: lavenderblush;"></div>
                
             </div>
+    
+
 
      
 
--- CustomerTestUma/Customer/Dashboard.aspx.designer.cs
+++ CustomerTestUma/Customer/Dashboard.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace CustomerTestUma.Customer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnBack;
+        
+        /// <summary>
+        /// btnBack1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBack1;
+        
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
